b60af413d2a5603921719b7cf4c1b166eb71447b
David Barsky <barskydavid@gmail.com>
2015-01-30 10:19:50 -0500

    Initial Commit

---
+++ GadaboutiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 1/30/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GadaboutiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 1/30/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
