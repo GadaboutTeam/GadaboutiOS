1a46c8a762bbd51114e69ae49a7ba0051c77a0ea
Alex Bardasu <abardasu@brandeis.edu>
2015-01-30 11:01:46 -0500

    Reorganized files.

---
+++ GadaboutiOS/LocationController.h
@@ -0,0 +1,15 @@
+//
+//  LocationController.h
+//  GadaboutiOS
+//
+//  Created by Alex Bardasu on 30/01/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef GadaboutiOS_LocationController_h
+#define GadaboutiOS_LocationController_h
+
+
+#endif
---
+++ GadaboutiOS/LocationController.m
@@ -0,0 +1,10 @@
+//
+//  LocationController.m
+//  GadaboutiOS
+//
+//  Created by Alex Bardasu on 30/01/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "LocationController.h"
+
