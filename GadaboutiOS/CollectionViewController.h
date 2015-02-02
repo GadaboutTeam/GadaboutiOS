88d3d23292b247dc795e8764db26428a9b319175
David Barsky <barskydavid@gmail.com>
2015-02-02 15:21:29 -0500

    added collectionview; remove viewcontroller; migrated locationcontroller

--- GadaboutiOS/CollectionViewController.h
+++ GadaboutiOS/CollectionViewController.h
@@ -1,18 +1,16 @@
 //
-//  ViewController.h
+//  CollectionViewController.h
 //  GadaboutiOS
 //
-//  Created by David Barsky on 1/30/15.
+//  Created by David Barsky on 2/2/15.
 //  Copyright (c) 2015 GadaboutTeam. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "LocationController.h"
 
-@interface ViewController : UIViewController {
+@interface CollectionViewController : UICollectionViewController {
     LocationController *locationController;
 }
 
-
 @end
-
