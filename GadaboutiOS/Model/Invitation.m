33a2b962ca04fbe2c4e5cc68149a3fb6082851a6
David Barsky <barskydavid@gmail.com>
2015-02-01 22:13:23 -0500

    created basic models

---
+++ GadaboutiOS/Model/Invitation.m
@@ -0,0 +1,27 @@
+//
+//  Invitation.m
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/1/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "Invitation.h"
+
+@implementation Invitation
+
+// Specify default values for properties
+
+//+ (NSDictionary *)defaultPropertyValues
+//{
+//    return @{};
+//}
+
+// Specify properties to ignore (Realm won't persist these)
+
+//+ (NSArray *)ignoredProperties
+//{
+//    return @[];
+//}
+
+@end
---
+++ GadaboutiOS/Model/Person.h
@@ -0,0 +1,19 @@
+//
+//  Person.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/1/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface Person : RLMObject
+
+@property NSString *name;
+
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<Person>
+RLM_ARRAY_TYPE(Person)
