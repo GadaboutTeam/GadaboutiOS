7e17470a8043230fa3d9a309d1ad261cbc4acac4
David Barsky <barskydavid@gmail.com>
2015-02-03 21:46:41 -0500

    added navigationbar view/set white status bar

---
+++ GadaboutiOS/NavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  NavigationBar.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/3/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationBar : UINavigationBar
+
+@end
