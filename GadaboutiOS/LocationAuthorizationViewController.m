219f7489a8878a84d6aa95bf02d3a3d7c51b1472
Alex Bardasu <abardasu@brandeis.edu>
2015-02-03 20:09:11 -0500

    Added location auth view controller

---
+++ GadaboutiOS/LocationAuthorizationViewController.m
@@ -0,0 +1,37 @@
+//
+//  LocationAuthorizationViewController.m
+//  GadaboutiOS
+//
+//  Created by Alex Bardasu on 03/02/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "LocationAuthorizationViewController.h"
+
+@interface LocationAuthorizationViewController ()
+
+@end
+
+@implementation LocationAuthorizationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
