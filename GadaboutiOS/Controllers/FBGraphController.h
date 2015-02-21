a5ff9cff68cfe956bacd9e5d6885d7f0ea09d540
David Barsky <barskydavid@gmail.com>
2015-02-20 19:48:25 -0500

    created Graph controller

---
+++ GadaboutiOS/Controllers/FBGraphController.h
@@ -0,0 +1,13 @@
+//
+//  FBGraphController.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/20/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBGraphController : NSObject
+
+@end
---
+++ GadaboutiOS/Controllers/FBGraphController.m
@@ -0,0 +1,13 @@
+//
+//  FBGraphController.m
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/20/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "FBGraphController.h"
+
+@implementation FBGraphController
+
+@end
