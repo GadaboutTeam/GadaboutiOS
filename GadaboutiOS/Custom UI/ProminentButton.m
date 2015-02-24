7ff99ac0836c836cb3e53c7af6d1056790f642bc
David Barsky <barskydavid@gmail.com>
2015-02-23 19:56:40 -0500

    subclasses UIButton to provide corner rounding

---
+++ GadaboutiOS/Custom UI/ProminentButton.m
@@ -0,0 +1,20 @@
+//
+//  ProminentButton.m
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/23/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "ProminentButton.h"
+
+
+@implementation ProminentButton
+
+-(void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [[self layer] setCornerRadius:4.0f];
+}
+
+@end
