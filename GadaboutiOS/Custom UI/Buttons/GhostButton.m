6c1563964ff67559c4348ccfc4aabbe6040ceb62
David Barsky <barskydavid@gmail.com>
2015-02-25 13:47:39 -0500

    setup animations for ghost button

--- GadaboutiOS/Custom UI/Buttons/GhostButton.m
+++ GadaboutiOS/Custom UI/Buttons/GhostButton.m
@@ -7,6 +7,7 @@
 //
 
 #import "GhostButton.h"
+#import <pop/POP.h>
 
 @implementation GhostButton
 
@@ -16,10 +17,45 @@
     [self setupButtonBorder];
 }
 
+#pragma mark - UIButton Overrides
+
 - (void)setupButtonBorder {
     [self.layer setBorderWidth:1.0f];
     [self.layer setBorderColor:[UIColor whiteColor].CGColor];
     [self.layer setCornerRadius: 4.0f];
+}
+
+#pragma mark - Animations
+
+- (void)touchUpInside:(GhostButton *)button {
+    
+}
+
+- (void)animationSetup {
+    [self addTarget:self action:@selector(scaleToSmall) forControlEvents:UIControlEventTouchDown | UIControlEventTouchDragEnter];
+    [self addTarget:self action:@selector(scaleAnimation) forControlEvents:UIControlEventTouchUpInside];
+    [self addTarget:self action:@selector(scaleToDefault) forControlEvents:UIControlEventTouchDragExit];
+    
+}
+
+- (void)scaleToSmall {
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(0.95f, 0.95f)];
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleSmallAnimation"];
+}
+
+- (void)scaleAnimation {
+    POPSpringAnimation *scaleAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.velocity = [NSValue valueWithCGSize:CGSizeMake(3.f, 3.f)];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.f, 1.f)];
+    scaleAnimation.springBounciness = 18.0f;
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleSpringAnimation"];
+}
+
+- (void)scaleToDefault {
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.f, 1.f)];
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleDefaultAnimation"];
 }
 
 @end
