964b755c15900229237b7d314df8eee6158708d8
David Barsky <barskydavid@gmail.com>
2015-02-26 13:51:23 -0500

    completed setup page layout

--- GadaboutiOS/Custom UI/Buttons/GhostButton.m
+++ GadaboutiOS/Custom UI/Buttons/GhostButton.m
@@ -23,8 +23,8 @@
 - (void)setupAppearence {
     // border
     [self.layer setBorderWidth:1.0f];
-    [self.layer setBorderColor:[UIColor whiteColor].CGColor];
-    [self.layer setCornerRadius: 4.0f];
+    [self.layer setBorderColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.8].CGColor];
+    [self.layer setCornerRadius: 5.0f];
     
     // background
     [self setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:.2]];
