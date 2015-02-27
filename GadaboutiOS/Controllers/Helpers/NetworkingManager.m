e7d85f6d15e8d085bc5fe5473c964a0347f7690d
David Barsky <barskydavid@gmail.com>
2015-02-27 10:45:51 -0500

    added NetworkingManager, subclass of AFHTTPSessionManager

---
+++ GadaboutiOS/Controllers/Helpers/NetworkingManager.m
@@ -0,0 +1,13 @@
+//
+//  NetworkingManager.m
+//  GadaboutiOS
+//
+//  Created by David Barsky on 2/27/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "NetworkingManager.h"
+
+@implementation NetworkingManager
+
+@end
