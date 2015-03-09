a9cb3a9e700f01f5350df27f3da81c7e1fb20e2b
David Barsky <barskydavid@gmail.com>
2015-03-08 23:24:24 -0400

    added table view under textfield

--- GadaboutiOS/Custom UI/Forms/TextField.m
+++ GadaboutiOS/Custom UI/Forms/TextField.m
@@ -12,12 +12,7 @@
 
 -(void)layoutSubviews {
     [super layoutSubviews];
-    [self setTextContainerInset:UIEdgeInsetsMake(13, 45, 0, 0)];
-}
-
-// for insetting the placeholder
-- (CGRect)textRectForBounds:(CGRect)bounds {
-    return CGRectInset(bounds, 15, 10);
+    [self setTextContainerInset:UIEdgeInsetsMake(13, 44, 0, 0)];
 }
 
 @end
