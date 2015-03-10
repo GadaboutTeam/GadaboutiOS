7883dcdb3e430a9a11de7353b378553415e43f5c
David Barsky <barskydavid@gmail.com>
2015-03-09 21:45:23 -0400

    remove collection view controller, replaced with table view. simplier

--- GadaboutiOS/Controllers/View Controllers/LocationAuthorizationViewController.h
+++ GadaboutiOS/Controllers/View Controllers/LocationAuthorizationViewController.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "CollectionViewController.h"
 #import "LocationController.h"
 
 @interface LocationAuthorizationViewController : UIViewController <LocationControllerDelegate> {
