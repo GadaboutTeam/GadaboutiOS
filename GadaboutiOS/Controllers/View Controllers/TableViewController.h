a389a3ac3a3d776017a334ff2765d39b8096049c
David Barsky <barskydavid@gmail.com>
2015-03-10 21:08:34 -0400

    created backing table view controller. still needs layout

---
+++ GadaboutiOS/Controllers/View Controllers/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 3/9/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
