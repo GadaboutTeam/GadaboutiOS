3ca39e8bd62c8cda5d4eab5a5aad7d6c9bdce983
David Barsky <barskydavid@gmail.com>
2015-03-26 17:08:19 -0400

    swapped out tableview for collection view for all conversatiosn

---
+++ GadaboutiOS/Controllers/View Controllers/ConversationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConversationsViewController.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 3/26/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConversationsViewController : UICollectionViewController
+
+@end
