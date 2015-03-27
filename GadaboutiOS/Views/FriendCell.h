718cb4c1f40a01fa90be91275ccccdb835ca1154
David Barsky <barskydavid@gmail.com>
2015-03-27 17:22:49 -0400

    created friend cell for collection view

---
+++ GadaboutiOS/Views/FriendCell.h
@@ -0,0 +1,13 @@
+//
+//  FriendCell.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 3/27/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendCell : UICollectionViewCell
+
+@end
---
+++ GadaboutiOS/Views/FriendCell.m
@@ -0,0 +1,13 @@
+//
+//  FriendCell.m
+//  GadaboutiOS
+//
+//  Created by David Barsky on 3/27/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "FriendCell.h"
+
+@implementation FriendCell
+
+@end
