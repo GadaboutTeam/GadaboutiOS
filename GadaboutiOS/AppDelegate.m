2100db9dca73b332b1ca53746b54ce383663f932
David Barsky <barskydavid@gmail.com>
2015-03-28 11:04:37 -0400

    add facebook login button

--- GadaboutiOS/AppDelegate.m
+++ GadaboutiOS/AppDelegate.m
@@ -8,8 +8,7 @@
 
 #import "AppDelegate.h"
 #import "LoginViewController.h"
-#import <Fabric/Fabric.h>
-#import <TwitterKit/TwitterKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import "NetworkingManager.h"
 #import "UserController.h"
 
@@ -43,10 +42,6 @@
 #pragma mark - Application Lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    [Fabric with:@[TwitterKit]];
-    [[Digits sharedInstance] logOut];
-    
     // for white text in navigation bar controllers
     [self setupAppearence];
     
@@ -54,9 +49,21 @@
     [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
     [[UIApplication sharedApplication] registerForRemoteNotifications];
 
-    [self setViewController];
+    if (![FBSDKAccessToken currentAccessToken]) {
+        // still needs to be filled out.
+    }
     
-    return YES;
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                    didFinishLaunchingWithOptions:launchOptions];
+}
+
+#pragma mark - Facebook
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation];
 }
 
 #pragma mark - Notication Token Setup
@@ -77,6 +84,8 @@
     [networkingManager sendDictionary:tokenDictionary toService:@"users"];
 }
 
+#pragma mark - Application Lifecycle
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -93,6 +102,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+        [FBSDKAppEvents activateApp];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
