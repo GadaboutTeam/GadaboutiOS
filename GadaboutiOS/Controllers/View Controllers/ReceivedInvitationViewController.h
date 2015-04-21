2f9104e5b5d660a8cf5d2d1dd623fca434c928d2
David Barsky <barskydavid@gmail.com>
2015-04-20 22:47:05 -0400

    add ReceivedInvitationViewController

---
+++ GadaboutiOS/Controllers/View Controllers/ReceivedInvitationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReceivedInvitationViewController.h
+//  GadaboutiOS
+//
+//  Created by David Barsky on 4/20/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceivedInvitationViewController : UIViewController
+
+@end
---
+++ GadaboutiOS/Controllers/View Controllers/ReceivedInvitationViewController.m
@@ -0,0 +1,13 @@
+//
+//  ReceivedInvitationViewController.m
+//  GadaboutiOS
+//
+//  Created by David Barsky on 4/20/15.
+//  Copyright (c) 2015 GadaboutTeam. All rights reserved.
+//
+
+#import "ReceivedInvitationViewController.h"
+
+@implementation ReceivedInvitationViewController
+
+@end
